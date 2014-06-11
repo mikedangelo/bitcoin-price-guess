83d3b8c9ff80834328eb13107281a04715319351
Ubuntu <ubuntu@ip-172-31-30-150.us-west-2.compute.internal>
2014-06-11 01:15:43 +0000

    starting the pipeline

---
+++ data/pipeline/extract/extract_bitstamp.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once("../../../web/constants.php");
+
+$eb = new Extract_Bitstamp();
+$eb->extract();
+var_dump($eb->extract());
+
+class Extract_Bitstamp {
+
+   public function extract() {
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, BITSTAMP_BTCUSD_TICKER);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      $output = curl_exec($ch);
+      curl_close($ch);     
+
+      return $output;
+   }
+}
+
+?>
---
+++ data/schema/schema_bitstamp.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Schema for bitstamp raw data
+ * 
+ * Note that we can refactor the basic deserializer (constructor) 
+ * and serializer (not written) into an abstract base for json feeds.
+ */
+class Schema_Bitstamp {
+
+   private $high = "";
+   private $last = "";
+   private $timestamp = "";
+   private $bid = "";
+   private $vwap = "";
+   private $volume = "";
+   private $low = "";
+   private $ask = "";
+
+   public function getHigh() {
+      return $this->high;
+   }
+   public function getLast() {
+      return $this->last;
+   }
+   public function getTimestamp() {
+      return $this->timestamp;
+   }
+   public function getBid() {
+      return $this->bid;
+   }
+   public function getVwap() {
+      return $this->vwap;
+   }
+   public function getVolume() {
+      return $this->volume;
+   }
+   public function getLow() {
+      return $this->low;
+   }
+   public function getAsk() {
+      return $this->ask;
+   }
+
+   public function setHigh($high) {
+      $this->high = $high;
+   }
+   public function setLast($last) {
+      $this->last = $last;
+   }
+   public function setTimestamp($timestamp) {
+      $this->timestamp = $timestamp;
+   }
+   public function setBid($bid) {
+      $this->bid = $bid;
+   }
+   public function setVwap($vwap) {
+      $this->vwap = $vwap;
+   }
+   public function setVolume($volume) {
+      $this->volume = $volume;
+   }
+   public function setLow($low) {
+      $this->low = $low;
+   }
+   public function setAsk($ask) {
+      $this->ask = $ask;
+   }
+
+}
