656f9c69f844a35ecbf4c3bd302b0356c5f2da13
Ubuntu <ubuntu@ip-172-31-30-150.us-west-2.compute.internal>
2014-06-18 06:25:25 +0000

    created the runner for bitstamp. will generalize in future. this inserts using pfs

--- data/pipeline/etl/extract_bitstamp.php
+++ data/pipeline/etl/extract_bitstamp.php
@@ -8,6 +8,17 @@ var_dump($eb->extract());
 
 class Extract_Bitstamp {
 
+   protected $feedid = 1;
+   protected $sleepyTime = 10;
+
+   public function getFeedId() {
+      return $this->feedid;
+   }
+
+   public function sleep() {
+      sleep($this->sleepyTime);
+   }
+
    public function extract() {
       $ch = curl_init();
       curl_setopt($ch, CURLOPT_URL, BITSTAMP_BTCUSD_TICKER);
